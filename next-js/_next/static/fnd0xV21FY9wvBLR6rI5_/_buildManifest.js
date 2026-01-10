faa41a3640aca4756545ba9647233a3bd31edddd
lazarusA <lazarus.alon@gmail.com>
2026-01-10 15:05:52 +0000

    Deploying to gh-pages from @ EarthyScience/netcdf4-wasm@6a8e15c3ae800b8c232021538232cc3d79c8f4df 🚀

---
+++ next-js/_next/static/fnd0xV21FY9wvBLR6rI5_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/next-js//_next/:path+",
+        "destination": "/next-js/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
