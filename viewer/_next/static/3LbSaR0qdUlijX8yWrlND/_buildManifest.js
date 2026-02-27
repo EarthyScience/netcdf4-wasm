cdaca3ad74c25a43f3be27c0d0da1cf501decc09
lazarusA <lazarus.alon@gmail.com>
2026-02-27 10:45:20 +0000

    Deploying to gh-pages from @ EarthyScience/netcdf4-wasm@45e97fd22dd0e3c9bc32e574361caa2c00aee21b 🚀

---
+++ viewer/_next/static/3LbSaR0qdUlijX8yWrlND/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/netcdf4-wasm/viewer//_next/:path+",
+        "destination": "/netcdf4-wasm/viewer/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
